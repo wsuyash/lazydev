f88a869a7f99aa78cb73db8142fab18a6bd432f8
wsuyash <suyashcr07@gmail.com>
2025-07-13 11:44:16 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="destructive">
+        Click me
+      </Button>
+    </div>
+  );
+}
