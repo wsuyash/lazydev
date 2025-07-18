8f105ccee46b4d719c3b36b01944659e4e9755b9
wsuyash <suyashcr07@gmail.com>
2025-07-18 19:30:47 +0530

    ai init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Page() {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
@@ -15,9 +19,13 @@ export default function Page() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "suyash" })}>
+
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
         Invoke Background Job
       </Button>
+
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.text}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert Next.js developer. You write readable, maintanable code. You write simeple Next.js & React snippets.",
+      model: gemini({ model: "gemini-2.0-flash-lite" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
