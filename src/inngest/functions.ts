36744f7e8153a643f1837ef4efdb412da1da876c
wsuyash <suyashcr07@gmail.com>
2025-07-18 21:04:01 +0530

    sandbox init

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,16 @@
 import { gemini, createAgent } from "@inngest/agent-kit";
-
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("lazydev-test-2");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-agent",
       system:
@@ -17,6 +22,11 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}`,
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      return sandbox.getHost(3000);
+    });
+
+    return { output, sandboxUrl };
   },
 );
